3e461f4c0e2f06657067a9594c4cae1d9e557309
Fedor <mydearfrodo@yandex.ru>
2017-05-09 01:55:50 +0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,149 @@
 // Логику пятнашек нужно описать в этом файле
+function shuffle(a) {
+  var j, x, i;
+  for (i = a.length; i; i--) {
+    j = Math.floor(Math.random() * i);
+    x = a[i - 1];
+    a[i - 1] = a[j];
+    a[j] = x;
+  }
+}
+
+function hideModal() {
+  var modal = document.getElementById('win-modal');
+  modal.style.display = 'none';
+}
+
+function printWin() {
+  var modal = document.getElementById('win-modal');
+  modal.style.display = "block";
+}
+
+function checkWin(board) {
+  var counter = 0;
+  var winnerPositions = [];
+  for (var i=1;i<16;i++) {
+    winnerPositions.push(i);
+  }
+  winnerPositions.push(0);
+  for (var i=0;i<4;i++) {
+    for (var j=0;j<4;j++) {
+      if (board[i][j] !== winnerPositions[counter]) {
+        return false;
+      } else {
+        counter++;
+      }
+    }
+  }
+  return true;
+}
+
+function canMoveCell(x, y) {
+  if (board[x + 1] && board[x + 1][y] === 0) {
+    board[x + 1][y] = board[x][y];
+    board[x][y] = 0;
+    return { type: 'bottom' , x: x + 1, y: y}
+  }
+  if (board[x - 1] && board[x - 1][y] === 0) {
+    board[x - 1][y] = board[x][y];
+    board[x][y] = 0;
+    return { type: 'top' , x: x - 1, y: y}
+  }
+  if (board[y + 1] && board[x][y + 1] === 0) {
+    board[x][y + 1] = board[x][y];
+    board[x][y] = 0;
+    return { type: 'right' , x: x, y: y + 1}
+  }
+  if (board[y - 1] && board[x][y - 1] === 0) {
+    board[x][y - 1] = board[x][y];
+    board[x][y] = 0;
+    return { type: 'left' , x: x, y: y - 1}
+  }
+}
+
+function clickOnBoard(event) {
+  var cellNode = event.target.closest('.cell');
+  var cell = cellNode.className.match(/ (.+)/)[1].split('-');
+  var move = canMoveCell(parseInt(cell[0]), parseInt(cell[1]))
+  var sign = 1;
+
+  if (move) {
+    switch(move.type) {
+      case 'left':
+        sign = -1;
+        break;
+      case 'right':
+        move.type = 'left';
+        break;
+      case 'top':
+        sign = -1;
+        break;
+      case 'bottom':
+        move.type = 'top';
+        break;
+    }
+    if (Number.isNaN(parseInt(cellNode.style[move.type]))) {
+      cellNode.style[move.type] = '0px';
+    }
+    cellNode.style[move.type] = parseInt(cellNode.style[move.type]) + (sign * 100) + 'px';
+    cellNode.className = 'cell ' + (move.x) + '-' + (move.y);
+    if (checkWin(board)) {
+      printWin();
+    }
+  } else {
+    cellNode.style.transform = 'rotate(360deg)';
+    setTimeout(function() {
+      cellNode.style.transform = '';
+    }, 300);
+  }
+}
+
+function printBoard(board, needMargin) {
+  var boardDiv = document.getElementsByClassName('board')[0];
+  boardDiv.innerHTML = '';
+  for (var i = 1; i <= 4; i++) {
+    for (var j = 1; j <= 4; j++) {
+      if (board[i - 1][j - 1] !== 0) {
+        var cell = document.createElement('div');
+        cell.onclick = clickOnBoard;
+        var indClass = (i - 1) + '-' + (j - 1);
+        cell.className = 'cell ' + indClass;
+        if (needMargin && i === 1 && j === 2) {
+          cell.style.marginLeft = '100px';
+        }
+        cell.innerHTML = '<div class="text-cell">' + board[i - 1][j - 1] + '</div>';
+        boardDiv.appendChild(cell);
+      }
+    }
+  }
+}
+
+var board = [];
+
+function startGame() {
+  hideModal();
+  var randoms = [];
+  for (var i = 1; i < 16; i++) {
+    randoms.push(i);
+  }
+  shuffle(randoms);
+  randoms.unshift(0);
+  board = []
+  for (var i=1;i<=4;i++) {
+    board[i - 1] = []
+    for (var j=1;j<=4;j++) {
+      board[i - 1][j - 1] = randoms[4 * (i - 1) + (j - 1)];
+    }
+  }
+  printBoard(board, true);
+}
+
+startGame();
+
+var reloadButton = document.getElementById('reload-game');
+
+reloadButton.onclick = function () {
+  hideModal();
+  startGame();
+};
+
--- tests/index-test.js
+++ tests/index-test.js
@@ -1,3 +1,60 @@
 describe('Tag', function() {
-    it('should ...');
+    afterEach(function() {
+        startGame();
+    })
+    it('should render board with 15 visible cells', function () {
+        var cells = document.getElementsByClassName('cell');
+        chai.expect(cells.length).to.be.equal(15);
+    });
+    it('should not render cell in first row and in first coll', function () {
+        var emptyCell = [].slice.call(document.getElementsByClassName('0-0'));
+        chai.expect(emptyCell).to.be.deep.equal([]);
+    });
+    it('should contain board with 15 cells and 1-15 numbers', function () {
+        var cells = document.getElementsByClassName('text-cell');
+        var numbers = [];
+        for (var i=1;i<16;i++) {
+            numbers.push(i);
+        }
+        var actualNumbers = [];
+        for (var i=0;i<cells.length;i++) {
+            actualNumbers.push(+cells[i].textContent);
+        }
+        actualNumbers.sort(function (a, b) {
+            return a - b;
+        });
+        chai.expect(actualNumbers).to.be.deep.equal(numbers);
+    });
+    it('should move cell left if clicked on first cell in first row', function () {
+        var firstCell = document.getElementsByClassName('0-1')[0];
+        firstCell.click();
+        chai.expect(firstCell.style.left).to.be.equal('-100px');
+    });
+    it('should not move not near with empty', function () {
+        var cell = document.getElementsByClassName('2-2')[0];
+        cell.click();
+        chai.expect(cell.style.left).to.be.equal('');
+        chai.expect(cell.style.top).to.be.equal('');
+    });
+    it('should move cell up if clicked on first cell in second row', function () {
+        var firstCell = document.getElementsByClassName('1-0')[0];
+        firstCell.click();
+        chai.expect(firstCell.style.top).to.be.equal('-100px');
+    });
+    it('should show modal if all numbers in correct positions and empty cell in last position', function () {
+        var board = [
+          [1, 2, 3, 4],
+          [5, 6, 7, 8],
+          [9, 10, 11, 12],
+          [13, 14, 15, 0],
+        ]
+        printBoard(board, false);
+        if (checkWin(board)) {
+            printWin();
+        }
+        var emptyCell = [].slice.call(document.getElementsByClassName('3-3'));
+        chai.expect(emptyCell).to.be.deep.equal([]);
+        var modal = document.getElementById('win-modal');
+        chai.expect(modal.style.display).not.to.be.equal('none');
+    });
 });
