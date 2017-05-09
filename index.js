a702de3d1606d1704a6131499c436cdfc8b35f19
Savi <Ultimate95@mail.ru>
2017-05-09 21:31:11 +0500

    init

--- index.js
+++ index.js
@@ -1,1 +1,155 @@
-// Логику пятнашек нужно описать в этом файле
+'use strict';
+
+var game = {
+    init: function () {
+        this._collectData();
+        this._bindEvenets();
+        this._initField();
+    },
+
+    _collectData: function () {
+        this.offset = 102;
+        this.$gameField = $('.js-game-field');
+        this.$rows = this.$gameField.children();
+        this.rowSelector = '.js-row';
+        this.cellSelector = '.js-cell';
+        this.spaceSelector = '.js-space';
+    },
+
+    _bindEvenets: function () {
+        this.$gameField.on('click', this.cellSelector, this._clickHandler.bind(this))
+    },
+
+    _initField: function () {
+        var $space = $(this.spaceSelector);
+
+        var cells = [];
+
+        this.$rows.each(function () {
+            $(this).children().each(function () {
+                if ($(this).get(0) !== $space.get(0)) {
+                    cells.push(this);
+                }
+            });
+        });
+
+        cells.sort(function () {
+            return .5 - Math.random();
+        });
+
+        cells.push($space);
+
+        this.$rows.each(function () {
+            var $row = $(this).empty();
+
+            var counter = 0;
+
+            $(cells).each(function () {
+                if (counter++ < 4) {
+                    $row.append(cells.pop());
+                } else {
+                    return false;
+                }
+            });
+
+        });
+
+        this.$gameField.animate({
+            opacity: 1
+        }, 1000)
+    },
+
+    _clickHandler: function (e) {
+        var $space = $(this.spaceSelector);
+
+        var elemRow = this._detectRow(e.target);
+        var spaceRow = this._detectRow($space);
+
+        var elemColumn = this._detectColumn(e.target);
+        var spaceColumn = this._detectColumn($space);
+
+        var offset;
+
+        if ((elemRow === spaceRow) && Math.abs(elemColumn - spaceColumn) === 1) {
+            offset = {
+                left: '+='
+            };
+
+            if (elemColumn - spaceColumn < 0) {
+                offset.left += this.offset;
+            } else {
+                offset.left += this.offset * -1;
+            }
+        }
+
+        if ((elemColumn === spaceColumn) && Math.abs(elemRow - spaceRow) === 1) {
+            offset = {
+                top: '+='
+            };
+
+            if (elemRow - spaceRow < 0) {
+                offset.top += this.offset;
+            } else {
+                offset.top += this.offset * -1;
+            }
+        }
+
+        if (offset) {
+            $(e.target).animate(offset, 300, function () {
+                this._swap(e.target, $space);
+                this._checkWin();
+            }.bind(this));
+        }
+    },
+
+    _detectRow: function (elem) {
+        var $elem = $(elem);
+        var $row = $elem.closest(this.rowSelector);
+        return $.inArray($row.get(0), this.$gameField.children()) + 1;
+    },
+
+    _detectColumn: function (elem) {
+        var $elem = $(elem);
+        var $row = $elem.closest(this.rowSelector);
+        return $.inArray($elem.get(0), $row.children()) + 1;
+    },
+
+    _checkWin: function () {
+        var currentCombination = [];
+
+        this.$rows.each(function () {
+            var $elem = $(this);
+            $elem.children().each(function () {
+                currentCombination.push(parseInt($(this).text(), 10));
+            });
+        });
+
+        var winCombination = currentCombination.slice();
+
+        winCombination.sort(function (a, b) {
+            return a - b;
+        });
+
+        if (currentCombination.toString() === winCombination.toString()) {
+            console.log('You won !')
+        }
+    },
+
+    _swap: function (elem_1, elem_2) {
+        var $elem_1 = $(elem_1);
+        var $elem_2 = $(elem_2);
+
+        var $copy_1 = $elem_1.clone();
+        var $copy_2 = $elem_2.clone();
+
+        $elem_1.replaceWith($copy_2);
+        $elem_2.replaceWith($copy_1);
+
+        $copy_1.removeAttr('style');
+        $copy_2.removeAttr('style');
+    }
+};
+
+$(function () {
+    game.init();
+});
--- tests/index-test.js
+++ tests/index-test.js
@@ -1,3 +1,28 @@
-describe('Tag', function() {
-    it('should ...');
+describe('Game tests', function () {
+    it('should init field', function () {
+        var rows = [].slice.call(document.getElementsByClassName('js-game-field')[0].children);
+
+        var counter = rows.reduce(function (counter, item) {
+            counter.elems ? counter.elems += item.children.length : counter.elems = item.children.length;
+
+            var elems = [].slice.call(item.children);
+
+            elems.forEach(function (item) {
+                counter.sum ? counter.sum += parseInt(item.textContent, 10) : counter.sum = parseInt(item.textContent, 10);
+            });
+
+            return counter;
+        }, {});
+
+        chai.assert.equal(counter.elems, 16);
+        chai.assert.equal(counter.sum, 120);
+    });
+
+    it('should move cell', function () {
+
+    });
+
+    it('should execute win action after move cell', function () {
+
+    });
 });
