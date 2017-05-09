cd443045b7c4bbde00ae784cfae8e996cea4634e
Ткач Даниил <Tkach@MacBook-Air-Tkac.local>
2017-05-10 00:18:07 +0500

    fix style; add test

--- index.js
+++ index.js
@@ -6,65 +6,6 @@ var app = (function() {
         var randomNums = nums.slice().sort(function() {
             return .5 - Math.random();
         });
-        var isValidCheck = function(el1, el2) {
-            var colEl1 = +el1.style.gridColumn[0];
-            var colEl2 = +el2.style.gridColumn[0];
-            var rowEl1 = +el1.style.gridRow[0];
-            var rowEl2 = +el2.style.gridRow[0];
-            return (Math.abs(colEl1 - colEl2) === 1 && Math.abs(rowEl1 - rowEl2) === 0)
-                || (Math.abs(colEl1 - colEl2) === 0 && Math.abs(rowEl1 - rowEl2) === 1);
-        }
-        var moveSpecksBlocks = function(el1, el2) {
-            var gridRow = el1.style.gridRow;
-            var gridCol = el1.style.gridColumn;
-            el1.style.gridRow = el2.style.gridRow;
-            el1.style.gridColumn = el2.style.gridColumn;
-            el2.style.gridRow = gridRow;
-            el2.style.gridColumn = gridCol;
-        }
-        var isGameFinish = function() {
-            var specksBlocks = document.querySelectorAll('.specks__item');
-            var specksItemNone = document.querySelector('.specks__item--hidden');
-            if (+specksItemNone.style.gridRow[0] !== 4 || +specksItemNone.style.gridColumn[0] !== 4)
-                return false
-            var flag = true;
-            for (var i = 1; i < specksBlocks.length; i++) {
-                if (+specksBlocks[i].innerHTML < 5 && (+specksBlocks[i].style.gridRow[0] !== 1
-                    ||  +specksBlocks[i].style.gridColumn[0] !== +specksBlocks[i].innerHTML))
-                    flag = false
-
-                if (+specksBlocks[i].innerHTML > 4 && +specksBlocks[i].innerHTML < 9
-                    && (+specksBlocks[i].style.gridRow[0] !== 2 ||  +specksBlocks[i].style.gridColumn[0] !== +specksBlocks[i].innerHTML - 4))
-                    flag = false
-
-                if (+specksBlocks[i].innerHTML > 8 && +specksBlocks[i].innerHTML < 13
-                    && (+specksBlocks[i].style.gridRow[0] !== 3 ||  +specksBlocks[i].style.gridColumn[0] !== +specksBlocks[i].innerHTML - 8))
-                    flag = false
-
-                if (+specksBlocks[i].innerHTML > 12 && +specksBlocks[i].innerHTML < 16
-                    && (+specksBlocks[i].style.gridRow[0] !== 4 ||  +specksBlocks[i].style.gridColumn[0] !== +specksBlocks[i].innerHTML - 12))
-                    flag = false
-            }
-            return flag
-        }
-
-        var gameFinish = function() {
-            var congratBlock = document.createElement('span');
-            congratBlock.innerHTML = 'You WIN!';
-            document.querySelector('.specks').appendChild(congratBlock);
-            var specksBlocks = document.querySelectorAll('.specks__item');
-            [].forEach.call(specksBlocks, function(item) {
-                item.className += ' game-finish';
-            });
-        }
-        var handlerClick = function(event) {
-            if (isGameFinish()) gameFinish()
-            else {
-                var specksItemNone = document.querySelector('.specks__item--hidden');
-                if (isValidCheck(event.target, specksItemNone))
-                    moveSpecksBlocks(event.target, specksItemNone);
-            }
-        }
         /** Add clear block */
         var specksItemNone = document.createElement('div');
         specksItemNone.className = 'specks__item specks__item--hidden';
@@ -83,6 +24,67 @@ var app = (function() {
             specksBlock.appendChild(newSpecksItem);
         })
         document.querySelector('.specks').appendChild(specksBlock);
+    }
+
+    var isValidCheck = function(el1, el2) {
+            var colEl1 = +el1.style.gridColumn[0];
+            var colEl2 = +el2.style.gridColumn[0];
+            var rowEl1 = +el1.style.gridRow[0];
+            var rowEl2 = +el2.style.gridRow[0];
+            return (Math.abs(colEl1 - colEl2) === 1 && Math.abs(rowEl1 - rowEl2) === 0)
+                || (Math.abs(colEl1 - colEl2) === 0 && Math.abs(rowEl1 - rowEl2) === 1);
+    }
+    var moveSpecksBlocks = function(el1, el2) {
+        var gridRow = el1.style.gridRow;
+        var gridCol = el1.style.gridColumn;
+        el1.style.gridRow = el2.style.gridRow;
+        el1.style.gridColumn = el2.style.gridColumn;
+        el2.style.gridRow = gridRow;
+        el2.style.gridColumn = gridCol;
+        }
+    var isGameFinish = function() {
+        var specksBlocks = document.querySelectorAll('.specks__item');
+        var specksItemNone = document.querySelector('.specks__item--hidden');
+        if (+specksItemNone.style.gridRow[0] !== 4 || +specksItemNone.style.gridColumn[0] !== 4)
+            return false
+        var flag = true;
+        for (var i = 1; i < specksBlocks.length; i++) {
+            if (+specksBlocks[i].innerHTML < 5 && (+specksBlocks[i].style.gridRow[0] !== 1
+                ||  +specksBlocks[i].style.gridColumn[0] !== +specksBlocks[i].innerHTML))
+                flag = false
+
+            if (+specksBlocks[i].innerHTML > 4 && +specksBlocks[i].innerHTML < 9
+                && (+specksBlocks[i].style.gridRow[0] !== 2 ||  +specksBlocks[i].style.gridColumn[0] !== +specksBlocks[i].innerHTML - 4))
+                flag = false
+
+            if (+specksBlocks[i].innerHTML > 8 && +specksBlocks[i].innerHTML < 13
+                && (+specksBlocks[i].style.gridRow[0] !== 3 ||  +specksBlocks[i].style.gridColumn[0] !== +specksBlocks[i].innerHTML - 8))
+                flag = false
+
+            if (+specksBlocks[i].innerHTML > 12 && +specksBlocks[i].innerHTML < 16
+                && (+specksBlocks[i].style.gridRow[0] !== 4 ||  +specksBlocks[i].style.gridColumn[0] !== +specksBlocks[i].innerHTML - 12))
+                flag = false
+        }
+        return flag
+    }
+
+    var gameFinish = function() {
+        var congratBlock = document.createElement('span');
+        congratBlock.className = 'specks__game-stop';
+        congratBlock.innerHTML = 'You WIN!';
+        document.querySelector('.specks').appendChild(congratBlock);
+        var specksBlocks = document.querySelectorAll('.specks__item');
+        [].forEach.call(specksBlocks, function(item) {
+            item.className += ' game-finish';
+        });
+    }
+    var handlerClick = function(event) {
+        if (isGameFinish()) gameFinish()
+        else {
+            var specksItemNone = document.querySelector('.specks__item--hidden');
+            if (isValidCheck(event.target, specksItemNone))
+                moveSpecksBlocks(event.target, specksItemNone);
+        }
     }
 
     return {
--- tests/index-test.js
+++ tests/index-test.js
@@ -1,3 +1,71 @@
-describe('Tag', function() {
-    it('should ...');
+describe('specks game test', function() {
+
+    afterEach(function() {
+        var specks = document.querySelector('.specks');
+        while (specks.firstChild) {
+            specks.removeChild(specks.firstChild);
+        }
+        app.init();
+    })
+
+    it('should generate 15 specks block', function() {
+        var specksBlocks = document.querySelectorAll('.specks__item');
+        var countBlock = 0;
+        [].forEach.call(specksBlocks, function(item) {
+            if (item.innerHTML !== '') countBlock += 1
+        })
+        chai.assert.equal(countBlock, 15);
+    });
+
+    it('should generate 1 specks hidden block', function() {
+        var specksItemNone = document.querySelector('.specks__item--hidden');
+        chai.assert.isDefined(specksItemNone, 'invisible block is defined');
+    });
+
+    it('should generate 15 specks block with value from 1 to 15', function() {
+        var specksBlocks = document.querySelectorAll('.specks__item');
+        var actual = [];
+        [].forEach.call(specksBlocks, function(item) {
+            if (item.innerHTML !== '' && +item.innerHTML < 16 && +item.innerHTML > 0 && actual.indexOf(+item.innerHTML) === -1)
+                actual.push(+item.innerHTML)
+        })
+
+        chai.assert.lengthOf(actual, 15, 'array is contained 15 blocks with value from 1 to 15');
+    });
+
+    it('should finish game when combination is [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]', function() {
+        var specksBlocks = document.querySelectorAll('.specks__item');
+        [].forEach.call(specksBlocks, function(item) {
+            if (item.innerHTML !== '') {
+                if (+item.innerHTML < 5) {
+                    item.style.gridColumn = +item.innerHTML;
+                    item.style.gridRow = 1;
+                }
+
+                if (+item.innerHTML > 4 && +item.innerHTML < 9) {
+                    item.style.gridRow = 2;
+                    item.style.gridColumn = +item.innerHTML - 4;
+                }
+
+                if (+item.innerHTML > 8 && +item.innerHTML < 13) {
+                    item.style.gridRow = 3;
+                    item.style.gridColumn = +item.innerHTML - 8;
+                }
+
+                if (+item.innerHTML > 12 && +item.innerHTML < 16) {
+                    item.style.gridRow = 4;
+                    item.style.gridColumn= +item.innerHTML - 12;
+                }
+            } else {
+                item.style.gridColumn = 4
+                item.style.gridRow = 4
+            }
+        })
+
+        specksBlocks[1].click();
+        var blockWin = document.querySelector('.specks__game-stop');
+        chai.assert.isDefined(blockWin, 'block win is defined');
+
+    });
+
 });
