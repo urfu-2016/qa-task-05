d2bc51620a13c2a8feb4fa45da0105f416486826
fLa <flatra66@gmail.com>
2017-05-10 01:34:16 +0500

    Implement game and tests

--- index.js
+++ index.js
@@ -1,1 +1,81 @@
-// Логику пятнашек нужно описать в этом файле
+var game = (function () {
+  function shuffleState (array) {
+    var arr = array.slice()
+    for (var i = arr.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1))
+      var temp = arr[i]
+      arr[i] = arr[j]
+      arr[j] = temp
+    }
+    return [0].concat(arr)
+  }
+
+  function startGame (state) {
+    $container.innerHTML = ''
+    state.forEach( function (i) {
+      var element = document.createElement('div')
+      element.className = 'element'
+      element.dataset.val = i
+      element.innerHTML = i
+      $container.appendChild(element)
+    })
+  }
+
+  function getElements () {
+    return Array.prototype.slice.call($container.children)
+  }
+
+  function checkVictory () {
+    var $nodes = getElements()
+    var isWin = true
+
+    for (var i = 0; i < $nodes.length; i++) {
+      if ($nodes[i].dataset.val != wonState[i]) {
+        isWin = false
+        break
+      }
+    }
+
+    if (isWin) {
+      restartGame()
+    }
+  }
+
+  function swapElements ($el) {
+    var $nodes = getElements()
+    var elIndex = $nodes.indexOf($el)
+    var $blank = $container.querySelector('[data-val="0"]')
+    var blankIndex = $nodes.indexOf($blank)
+
+    if ([elIndex + 1, elIndex - 1, elIndex + 4, elIndex - 4].includes(blankIndex)) {
+      var $clonedEl = $el.cloneNode(true)
+      var $clonedBlank = $blank.cloneNode(true)
+
+      $blank.parentNode.replaceChild($clonedEl, $blank)
+      $el.parentNode.replaceChild($clonedBlank, $el)
+
+      checkVictory()
+    }
+  }
+
+  function restartGame () {
+    alert('Победа! Следущая игра начнется автоматически')
+    startGame(shuffleState(digits))
+  }
+
+  var $container = document.getElementById('puzzle')
+  var digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+  var wonState = digits.concat([0])
+  startGame(shuffleState(digits))
+
+  $container.onclick = function (e) {
+    swapElements(e.target)
+  }
+
+  return {
+    getElements: getElements,
+    $container: $container,
+    start: startGame,
+    restart: restartGame
+  }
+}())
--- tests/index-test.js
+++ tests/index-test.js
@@ -1,3 +1,85 @@
-describe('Tag', function() {
-    it('should ...');
-});
+var assert = chai.assert
+
+describe('Puzzle', function () {
+  after( function () {
+    game.restart()
+    window.alert.restore()
+  })
+
+  it('Runs new game onload', function () {
+    $elements = game.getElements()
+    assert.lengthOf($elements, 16)
+    $elements.forEach(function (el) {
+      assert.equal(el.classList, 'element')
+      assert.equal(el.tagName, 'DIV')
+      assert.equal(el.dataset.val, el.textContent)
+    })
+  });
+
+  describe('Swaps', function () {
+    var $elements, state
+
+    beforeEach(function () {
+      state = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 14]
+      game.start(state)
+      $elements = game.getElements()
+    });
+
+    it('Blank with adjacent horizontal element', function () {
+      var blank = $elements[0]
+      var first = $elements[1]
+
+      assert.equal(blank.dataset.val, '0')
+      first.click()
+
+      $elements = game.getElements()
+
+      first = $elements.find( function (el) { return el.dataset.val == '1' })
+      blank = $elements.find( function (el) { return el.dataset.val == '0' })
+
+      assert.equal($elements.indexOf(blank), 1)
+      assert.equal($elements.indexOf(first), 0)
+    });
+
+    it('Blank with adjacent vertical element', function () {
+      var blank = $elements[0]
+      var four = $elements[4]
+
+      assert.equal(blank.dataset.val, '0')
+      four.click()
+
+      $elements = game.getElements()
+
+      four = $elements.find( function (el) { return el.dataset.val == '4' })
+      blank = $elements.find( function (el) { return el.dataset.val == '0' })
+
+      assert.equal($elements.indexOf(blank), 4)
+      assert.equal($elements.indexOf(four), 0)
+    });
+
+    it('Nothing if there were no adjacent blank element', function () {
+      $elements[10].click()
+
+      var notChanged = true
+      $elements = game.getElements()
+      for (var i = 0; i < $elements.length; i++) {
+        if ($elements[i].dataset.val != state[i]) {
+          notChanged = false
+          break
+        }
+      }
+
+      assert(notChanged)
+    })
+  })
+
+  it('Shows alert on win', function () {
+    var spy = sinon.stub(window, 'alert')
+    var state = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 0, 15]
+    game.start(state)
+    game.getElements()[$elements.length - 1].click()
+
+    assert(spy.calledOnce)
+    assert.equal(spy.args[0][0], 'Победа! Следущая игра начнется автоматически')
+  })
+})
