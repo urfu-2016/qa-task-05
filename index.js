e5745d8c37adb6d390414af86e73ef224485d43e
nikitc <nikitc6@rambler.ru>
2017-05-09 22:31:18 +0500

    Добавлено решение

--- index.js
+++ index.js
@@ -1,1 +1,109 @@
-// Логику пятнашек нужно описать в этом файле
+'use strict';
+
+var SIZE_FIELD = 4;
+var CURRENT_COMBINATION = [];
+var WINNER_COMBINATION = [];
+
+
+function createGame() {
+    document.getElementById('start-game-button').onclick = function () {
+        clear();
+        createGame();
+    };
+    WINNER_COMBINATION = getWinnerCombination();
+    CURRENT_COMBINATION = shuffle(WINNER_COMBINATION.slice());
+    fillGameField();
+}
+
+function clear() {
+    document.getElementById('game-field').innerHTML = '';
+    document.getElementById('result').innerHTML = '';
+}
+
+function fillGameField() {
+    var field = document.getElementById('game-field');
+    CURRENT_COMBINATION.forEach(function (elem, index) {
+        field.appendChild(createChip(elem, index));
+    });
+}
+
+function render() {
+    clear();
+    fillGameField()
+}
+
+function createChip(number, index) {
+    var chip = document.createElement('div');
+    chip.innerHTML = number + '';
+    chip.className = number ? 'chip' : 'chip zero';
+    if (number) {
+        chip.onclick = cellClick.bind(this, index);
+    }
+
+    return chip;
+}
+
+function isEmpty(index) {
+    return (index >= 0) &&
+        (index < SIZE_FIELD*SIZE_FIELD) &&
+        (CURRENT_COMBINATION[index] === 0);
+}
+
+function swap(indexFrom, indexTo) {
+    var temp = CURRENT_COMBINATION[indexFrom];
+    CURRENT_COMBINATION[indexFrom] = CURRENT_COMBINATION[indexTo];
+    CURRENT_COMBINATION[indexTo] = temp;
+}
+
+function cellClick(index) {
+    if (isEmpty(index + 1)) {
+        swap(index, index + 1);
+        render();
+    }
+
+    if (isEmpty(index - 1)) {
+        swap(index, index - 1);
+        render();
+    }
+
+    if (isEmpty(index + 4)) {
+        swap(index, index + 4);
+        render();
+    }
+
+    if (isEmpty(index - 4)) {
+        swap(index, index - 4);
+        render();
+    }
+
+    if (isWin()) {
+        document.getElementById('result').innerHTML = 'Вы выиграли!';
+    }
+}
+
+function isWin() {
+    return JSON.stringify(WINNER_COMBINATION) === JSON.stringify(CURRENT_COMBINATION);
+}
+
+function getWinnerCombination() {
+    var combination = [];
+    for (var i = 1; i < SIZE_FIELD*SIZE_FIELD; i++) {
+        combination.push(i);
+    }
+    combination.push(0);
+
+    return combination;
+}
+
+function shuffle(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    return array;
+}
+
+createGame();
--- tests/index-test.js
+++ tests/index-test.js
@@ -1,3 +1,56 @@
-describe('Tag', function() {
-    it('should ...');
+chai.should();
+
+function assertChips() {
+    var gameField = document.getElementById('game-field');
+    gameField.childElementCount.should.be.equal(SIZE_FIELD*SIZE_FIELD);
+
+    for (var i = 0; i < SIZE_FIELD*SIZE_FIELD; i++) {
+        var currentChip = gameField.childNodes[i];
+        currentChip.innerHTML.should.be.equal(CURRENT_COMBINATION[i].toString());
+        currentChip.className.should.be.contains('chip');
+    }
+}
+
+describe('Game ui tests', function() {
+    it('should create game', function () {
+        assertChips();
+    });
+
+    it('should render game when chip move', function () {
+        var gameField = document.getElementById('game-field');
+        var indexEmpty = CURRENT_COMBINATION.indexOf(0);
+        var empty = gameField.childNodes[indexEmpty];
+        empty.click();
+
+        assertChips();
+    });
+
+    it('should render game when chip move', function () {
+        var gameField = document.getElementById('game-field');
+        var indexEmpty = CURRENT_COMBINATION.indexOf(0);
+        var empty = gameField.childNodes[indexEmpty];
+        empty.click();
+
+        assertChips();
+    });
+
+    it('should render "result" is empty', function () {
+        document.getElementById('game-field').childNodes[2].click();
+        var result = document.getElementById('result');
+        result.innerText.should.be.not.equal('Вы выиграли!');
+    });
+
+    it('should render "Вы выиграли!" when game complete', function () {
+        CURRENT_COMBINATION = [1, 2, 3, 4,
+                               5, 6, 7, 8,
+                               9, 10, 11, 12,
+                               13, 14, 0, 15];
+        render();
+        document.getElementById('game-field').childNodes[15].click();
+
+        var result = document.getElementById('result');
+        result.innerText.should.be.equal('Вы выиграли!');
+        document.getElementById('start-game-button').click();
+    })
+
 });
