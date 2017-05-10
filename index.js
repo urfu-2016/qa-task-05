30da9c6ba0bf2a078e65e908e5db206b66687756
AnastasiaBabushkina <nastena.babushkina.96@mail.ru>
2017-05-10 18:07:14 +0500

    ''

--- index.js
+++ index.js
@@ -1,1 +1,69 @@
 // Логику пятнашек нужно описать в этом файле
+// Логику пятнашек нужно описать в этом файле
+var VICTORY = [];   
+var CURRENT_PLAY = [];
+
+
+function newGame() {
+    document.getElementById('board').innerHTML = '';
+    document.getElementById('victory').innerHTML = '';
+    var cells = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0];
+    VICTORY = cells.slice();   
+    CURRENT_PLAY = mix(cells);
+    draw();
+}
+
+function mix(arr) {
+    return arr.sort(function() { 
+        return Math.random() - 0.5; 
+    })
+}
+
+function draw() {
+    var drawCell = document.getElementById('board');
+    CURRENT_PLAY.forEach(function (elem, index) {
+        var cell = document.createElement('div');
+        cell.innerHTML = elem + '';
+        cell.className = elem ? 'cell' : 'cell empty';
+        if (elem) {
+            cell.onclick = movement.bind(this, index);
+            checkVictory();
+        }
+        drawCell.appendChild(cell);
+    });
+}
+
+function checkVictory() {
+    if (JSON.stringify(VICTORY) === JSON.stringify(CURRENT_PLAY)) {
+        document.getElementById('victory').innerHTML = 'Победа!';}
+}
+
+
+function movement(index) {
+    if (CURRENT_PLAY[index + 1] === 0) {
+        moveCell(index, index + 1);
+    }
+
+    if (CURRENT_PLAY[index - 1] === 0) {
+        moveCell(index, index - 1);
+    }
+
+    if (CURRENT_PLAY[index + 4] === 0) {
+        moveCell(index, index + 4);
+    }
+
+    if (CURRENT_PLAY[index - 4] === 0) {
+        moveCell(index, index - 4);
+    }
+}
+
+function moveCell(from, to) {
+    var emptyCell = CURRENT_PLAY[from];
+    CURRENT_PLAY[from] = CURRENT_PLAY[to];
+    CURRENT_PLAY[to] = emptyCell;
+    document.getElementById('board').innerHTML = '';
+    document.getElementById('victory').innerHTML = '';
+    draw();
+}
+
+newGame();
--- tests/index-test.js
+++ tests/index-test.js
@@ -1,3 +1,48 @@
+chai.should();
+
+
 describe('Tag', function() {
-    it('should ...');
+	beforeEach(function () {
+		newGame();
+    });
+
+    it('check the format of the cells', function () {
+    	document.getElementById('board').childElementCount.should.be.equal(16);
+
+	    for (var i = 1; i < 16; i++) {
+	        var currentcell = document.getElementById('board').childNodes[i];
+	        currentcell.innerHTML.should.be.equal(CURRENT_PLAY[i].toString());
+	        currentcell.className.should.be.contains('cell');
+	    }
+    });
+
+    it('must draw cell movement', function () {
+        var indexEmpty = CURRENT_PLAY.indexOf(0);
+        var empty = document.getElementById('board').childNodes[indexEmpty];
+        empty.click();
+    });
+
+    it('should not draw a "Победа!"', function () {
+        document.getElementById('board').childNodes[2].click();
+        var victory = document.getElementById('victory');
+        victory.innerText.should.be.not.equal('Победа!');
+    });
+
+    it('should draw "Победа!" when game victory', function () {
+    	try {
+	    	newGame();
+	        CURRENT_PLAY = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 0, 15];
+		    draw();
+	        document.getElementById('board').childNodes[15].click();
+
+	        var victory = document.getElementById('victory');
+	        victory.innerText.should.be.equal('Победа!');
+    	}
+    	catch (err) {
+    		 throw err;
+    	} finally {
+    		newGame();
+    	}
+    })
+
 });
