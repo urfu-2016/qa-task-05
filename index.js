d740f245120c648d1964b859c80083fcc081eedb
Antonov Alexey <aleksei193@mail.ru>
2017-05-10 16:28:09 +0500

    Решил задачу

--- index.js
+++ index.js
@@ -1,1 +1,131 @@
-// Логику пятнашек нужно описать в этом файле
+function getCombination() {
+    var combination = [];
+    for (var i = 1; i < 16; i++) {
+        combination.push(i);
+    }
+
+    combination.sort(function () {
+        return 0.5 - Math.random();
+    });
+
+    return [16].concat(combination);
+}
+
+function generateCombination() { 
+    var combination;
+    var inv = 1;
+
+    while (inv % 2 !== 0) {
+        inv = 0;
+        combination = getCombination();
+
+        for (var i = 0; i < 16; i++) {
+            for (var j = 0; j < i; j++) {
+                if (combination[j] > combination[i]) {
+                    inv++;
+                }
+            }
+        }
+    }
+
+    return combination;
+}
+
+function getPosition(i) {
+    return {row: Math.floor(i / 4), column:  i % 4};
+}    
+
+function createCell(value, i) {
+    var cell = document.createElement('div');
+    cell.className = 'block cell';
+    
+    var xy = getPosition(i);
+    cell.style.top = (25 * xy.row + 1) + '%'; 
+    cell.style.left = (25 * xy.column + 1) + '%';
+    if (value === 16) {
+        cell.style.visibility = 'hidden';
+    } else {
+        cell.innerHTML = value;
+    }
+
+    return cell;
+}
+    
+function createWinMessage() {
+    var div = document.createElement('div');
+    div.innerHTML = "You win";
+    div.className = 'block win-message';
+    
+    return div;
+}
+
+function isNeighbors(i, j) {
+    var p1 = getPosition(i);
+    var p2 = getPosition(j);
+    return Math.abs(p1.row - p2.row) + Math.abs(p1.column - p2.column) === 1;
+}    
+
+function swap(array, i, j) {
+    var e = array[i];
+    array[i] = array[j];
+    array[j] = e;
+}
+    
+function swapCellStyle(cellOne, cellTwo, property) {
+    var styleProperty = cellOne.style[property];
+    cellOne.style[property] = cellTwo.style[property];
+    cellTwo.style[property] = styleProperty;
+}
+
+function isWin(combination) {
+    for (var i = 1; i < combination.length; i++) {
+        if (combination[i-1] > combination[i]) {
+            return false;
+        }
+    }     
+
+    return true;
+}
+
+var combination;
+
+function init() {
+    combination = generateCombination();
+    var cells = combination.map(createCell);
+    var emptyCellIndex = combination.indexOf(16);
+    var game = document.getElementById('game');
+    
+    game.innerHTML = "";
+    
+    cells.forEach(function(cell) {
+        game.appendChild(cell);
+    });
+    
+    game.className = 'cells';
+    game.onclick = function(e) {
+        event = e || window.event;
+        var div = event.target || event.srcElement;
+        
+        if (!div.classList.contains('cell')) {
+            return;
+        }
+        
+        var cellIndex = cells.indexOf(div);
+        if (isNeighbors(cellIndex, emptyCellIndex)) {
+                swap(cells, cellIndex, emptyCellIndex);
+                swap(combination, cellIndex, emptyCellIndex);
+                swapCellStyle(cells[cellIndex], cells[emptyCellIndex], 'top');
+                swapCellStyle(cells[cellIndex], cells[emptyCellIndex], 'left');
+                emptyCellIndex = cellIndex;
+            if (isWin(combination)) {
+                game.appendChild(createWinMessage());
+                game.onclick = "";
+                cells.forEach(function(cell) {
+                    cell.style.opacity = '.5';
+                });
+            }
+        }
+    };
+}
+    
+document.body.onload = init();
--- tests/index-test.js
+++ tests/index-test.js
@@ -1,3 +1,183 @@
-describe('Tag', function() {
-    it('should ...');
+var expect = chai.expect;
+
+function getNeighborsIndexes(index) {
+    var neighborIndexes = [];
+    
+    var position = getPosition(index);
+
+    if (position.row < 3) {
+        neighborIndexes.push(index + 4);
+    }
+
+    if (position.row > 0) {
+        neighborIndexes.push(index - 4);
+    }
+    
+    if (position.column > 0) {
+        neighborIndexes.push(index - 1);
+    }
+    
+    if (position.column < 3) {
+        neighborIndexes.push(index + 1);
+    }
+
+    return neighborIndexes;
+}
+
+function getNotNeighborsIndex(index) {
+    var neighborIndexes = getNeighborsIndexes(index);
+    var notNeighborIndex;
+    
+    for(var i = 0; i < 16; i++) {
+        if (neighborIndexes.indexOf(i) === -1 && i !== index) {
+            notNeighborIndex = i;
+            break;
+        } 
+    }
+
+    return notNeighborIndex;
+}
+
+function getHiddenCellIndex(cells) {
+    for(var i = 0; i < cells.length; i++) {
+        if (cells[i].style.visibility === 'hidden') {
+            return i;
+        }
+   }
+
+   return -1;
+}
+
+function getElements(game, className) {
+    return [].filter.call(game.children, function(elem) {
+        return elem.classList.contains(className);
+    });
+}
+
+function getCellPosition(cell) {
+    return {
+        top: cell.style.top,
+        left: cell.style.left
+    }
+}
+
+function testPosition(actual, expected) {
+    expect(actual.top).to.equal(expected.top);
+    expect(actual.left).to.equal(expected.left);
+}
+
+describe('getNeighborsIndexes', function() {
+    it('should return [1, 4] when index is 0', function () {
+        var actual = getNeighborsIndexes(0);
+
+        expect(actual).to.include.members([1, 4])
+    });
+    
+    it('should return [0, 2, 5] when index is 1', function () {
+        var actual = getNeighborsIndexes(1);
+
+        expect(actual).to.include.members([0, 2, 5])
+    });
+
+    it('should return [1, 4, 6, 9] when index is 5', function () {
+        var actual = getNeighborsIndexes(5);
+
+        expect(actual).to.include.members([1, 4, 6, 9])
+    });
+    
+});  
+
+describe('Game', function() {
+    var event = new Event('click', {bubbles: true});
+    var cells;
+    
+    beforeEach(function () {
+        var game = document.getElementById('game');
+        init();
+        cells = getElements(game, 'cell');
+    });
+    
+    after(function () {
+        init();
+    });
+
+    context('Initialization', function () {
+        it('should create 16 cells', function () {
+            expect(cells).to.have.lengthOf(16);
+        });
+        
+        it('should create 15 visibable cells', function () {
+            var visibleCells = cells.filter(function(cell) {
+                return cell.style.visibility === '';
+            });
+
+            expect(visibleCells).to.have.lengthOf(15);
+        });
+        
+        it('should create 1 hidden cell in position (1%, 1%)', function () {
+            var hiddenCells = cells.filter(function(cell) {
+                return cell.style.visibility === 'hidden';
+            });
+
+            expect(hiddenCells).to.have.lengthOf(1);
+            testPosition(hiddenCells[0].style, {top: '1%', left: '1%'});
+        });
+    });
+
+    context('Move', function () {
+        var indexOfhiddenCell;
+
+        beforeEach(function () {
+            indexOfhiddenCell = getHiddenCellIndex(cells);
+        });
+
+        it('shouldn\'t move cells when click on empty cell', function () {
+            var positions = cells.map(getCellPosition);
+            cells[indexOfhiddenCell].dispatchEvent(event);
+
+            for (var i = 0; i < cells.length; i++) {
+                testPosition(cells[i].style, positions[i])
+            }
+        });
+
+        it('should move cell when click on neighbore of empty cell', function () {
+            var neighborIndex = getNeighborsIndexes(indexOfhiddenCell)[0];
+
+            var neighboreCellProperties = getCellPosition(cells[neighborIndex]);
+            var hiddenCellProperties = getCellPosition(cells[indexOfhiddenCell]);
+
+            cells[neighborIndex].dispatchEvent(event);
+
+            testPosition(cells[indexOfhiddenCell].style, neighboreCellProperties);
+            testPosition(cells[neighborIndex].style, hiddenCellProperties)
+        });
+
+        it('shouldn\'t move cells when click on not neighbors of empty cell', function () {
+            var notNeighborIndex = getNotNeighborsIndex(indexOfhiddenCell);
+
+            var positions = cells.map(getCellPosition);
+            cells[notNeighborIndex].dispatchEvent(event);
+
+            for (var i = 0; i < cells.length; i++) {
+                testPosition(cells[i].style, positions[i])
+            }
+        });
+    });
+
+    context('Win', function () {
+        it('should show win message', function () {
+            var winCombination = [16, 1, 2, 3, 5, 6, 7, 4, 9, 10, 11, 8, 13, 14, 15, 12];
+            combination = winCombination;
+            
+            winMessages = getElements(game, 'win-message');
+            expect(winMessages).to.have.lengthOf(0);
+            
+            [1, 2, 3, 7, 11, 15].forEach(function(i) {
+                cells[i].dispatchEvent(event);
+            });
+            
+            winMessages = getElements(game, 'win-message');
+            expect(winMessages).to.have.lengthOf(1);
+        });
+    });
 });
