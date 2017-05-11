55db199333757c1f0bda40bdc19088b29ad94a2b
NeSmogPridymatNick <IceDruid@yandex.ru>
2017-05-11 19:59:40 +0500

    Update index-test.js

--- tests/index-test.js
+++ tests/index-test.js
@@ -4,38 +4,25 @@ describe('Игра', function() {
     });
     it('Должен показывать игровое поле', function () {
         startGame();
-        for(var i = 0; i < 4; i++) {
+        for(var i = 0; i < SIZE*SIZE; i++) {
             chai.expect(document.getElementById('pole' + i)).to.exist;
         }
         deleTe();
     });
-    it('Перемещает квадратик', function () {
-        startGame();
-        var but = document.getElementById("but1");
-        but.checked = true;
-        var pole = move(0,0);
-        chai.assert.equal(pole[0][0], 1);
-        chai.assert.equal(pole[0][1], 0);
-        deleTe();
-    });
     it('При попутки переместить квадратик без соседства с пустым, ничего не происходит', function () {
         startGame();
-        var but = document.getElementById("but3");
-        but.checked = true;
-        var pole = move(0,0);
-        chai.assert.equal(pole[0][0], 0);
-        chai.assert.equal(pole[1][1], 3);
-        deleTe();
-    });
-    it('После конца игры появляется сообщение WIN', function () {
-        startGame();
-        var but = document.getElementById("but1");
-        but.checked = true;
+        var a = POLE[0][0];
         move(0,0);
-        but = document.getElementById("but3");
-        but.checked = true;
-        move(0,1);
-        chai.assert.equal(win(), true);
+        chai.assert.equal(POLE[0][0], a);
         deleTe();
     });
+    it('Перемещает квадратик', function () {
+        startGame();
+        var a = POLE[3][2];
+        move(2, 3);
+        chai.assert.equal(POLE[3][3], a);
+        chai.assert.equal(POLE[3][2], 0);
+        deleTe();
+    });
+
 });
