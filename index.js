7ccbc9c29cde046079df47104edfe2ddfab58f6c
NeSmogPridymatNick <IceDruid@yandex.ru>
2017-05-11 19:59:10 +0500

    Исправления
    Отступы
    Игровое поле с 2*2 до 4*4
    Правило передвижение квадратика:
    Раньше - выбираем квадратик(1 клик) , потом куда хотим передвинуть(2 клик).
    Сейчас - Если рядом с квадратиком есть пустое поле, то по 1 клику он переместиться.

--- index.js
+++ index.js
@@ -1,137 +1,149 @@
-// Логику пятнашек нужно описать в этом файле
-var pole = [[3 ,1],
-            [2 ,0]];
+var SIZE = 4;
+var recSize = 100;
+var POLE = [];
 function getCor(event) {
-            var el = document.getElementById('game');
-            var pX = Math.round((event.clientX - findPosX(el))/110 - 0.5);
-            var pY = Math.round((event.clientY - findPosY(el))/105 - 0.5);
-            if (pX === 2) {
-                pX = 1;
-            }
-            if (pY ===2) {
-                pY = 1;
-            }
-            move(pX, pY);
-            if (win()) {
-                alert("WIN");
-            }
-        }
-        function findNum(num) {
-            for(var i = 0; i < pole.length; i++) {
-                for(var j = 0; j < pole.length; j++) {
-                    if (pole[i][j] === num) {
-                        return [i, j];
-                    }
-                }
+    var el = document.getElementById('game');
+    var pX = Math.trunc((event.clientX - findPosX(el))/recSize);
+    var pY = Math.trunc((event.clientY - findPosY(el))/recSize);
+    move(pX, pY);
+    if (win()) {
+        alert("You win!");
+    }
+}
+function findNum(num) {
+    for(var i = 0; i < SIZE; i++) {
+        for(var j = 0; j < SIZE; j++) {
+            if (POLE[i][j] === num) {
+                return [i, j];
             }
         }
-        function deleTe() {
-            var game = document.getElementById('game');
-            for(var i = 0; i < 4; i++) {
-                game.removeChild(document.getElementById('pole'+i));
+    }
+}
+function win() {
+    var w = 1;
+    for(var i = 0; i < SIZE; i++) {
+        for(var j = 0; j < SIZE; j++) {
+            if (POLE[i][j] === w) {
+                w++
+            } else {
+                return false
             }
         }
-        function swapDiv(x, y, nameDiv, numDiv) {
-            var two = findNum(numDiv);
-            pole[x][y] = numDiv;
-            pole[two[0]][two[1]] = 0;
-            var d1=document.getElementById(nameDiv);
-            var d2=document.getElementById("pole0");
-            var d11=d1.cloneNode(true);
-            var d22=d2.cloneNode(true);
-            d2.parentNode.insertBefore(d11,d2);
-            d1.parentNode.insertBefore(d22,d1);
-            d1.parentNode.removeChild(d1);
-            d2.parentNode.removeChild(d2);
+    }
+    return true;
+}
+function deleTe() {
+    var game = document.getElementById('game');
+    for(var i = 0; i < SIZE*SIZE; i++) {
+        game.removeChild(document.getElementById('pole'+i));
+    }
+}
+function swapDiv(x, y, nameDiv, numDiv) {
+    var two = findNum(numDiv);
+    POLE[y][x] = numDiv;
+    POLE[two[0]][two[1]] = 0;
+    var d1=document.getElementById(nameDiv);
+    var d2=document.getElementById("pole0");
+    var d11=d1.cloneNode(true);
+    var d22=d2.cloneNode(true);
+    d2.parentNode.insertBefore(d11,d2);
+    d1.parentNode.insertBefore(d22,d1);
+    d1.parentNode.removeChild(d1);
+    d2.parentNode.removeChild(d2);
 
-        }
-        function notDiag(x, y, num) {
-            if ((x === y && findNum(num)[0] === findNum(num)[1]) ||
-            (x === findNum(num)[1] && findNum(num)[0] === y)) {
-                return false;
-            }
-            return true;
-        }
-        function move(x, y) {
-            var but1 = document.getElementById('but1');
-            var but2 = document.getElementById('but2');
-            var but3 = document.getElementById('but3');
-            if(pole[x][y] === 0) {
-                if (but1.checked && notDiag(x, y, 1)) {
-                    swapDiv(x, y, "pole1", 1);
-                }
-                if (but2.checked && notDiag(x, y, 2)) {
-                    swapDiv(x,y,"pole2",2);
-                }
-                if (but3.checked && notDiag(x, y, 3)) {
-                    swapDiv(x,y,"pole3",3);
-                }
-            }
-            return pole;
-        }
-        function win() {
-            if(pole[0][0] === 1 && pole[0][1] === 3 && pole[1][0] === 2 ) {
-                return true;
-            }
-            return false;
-        }
-        function startGame() {
-            pole = [[3 ,1],
-                    [2 ,0]];
-            addElements();
-            swapDiv(1,1,"pole3",3);
-        }
-        function addElements() {
-            var my_div = document.getElementById("game");
-            for(var i=3; i>=0; i--) {
-                var radio = document.createElement("input");
-                radio.name = 'polygon';
-                radio.className = "active" + i;
-                radio.type = 'radio';
-                radio.id = "but" + i;
-                var div = document.createElement("div");
-                div.className = "point" + i;
-                if (i > 0) {
-                    div.innerHTML = "<h1>" + i + "</h1>";
-                }
-                div.id = 'pole' + i;
-                var label = document.createElement("label");
-                label.htmlFor = "but" + i;
-                if(i > 0) {
-                    div.appendChild(label);
-                    document.body.insertBefore(radio, my_div);
-                }
-                my_div.appendChild(div);
+}
+function move(x, y) {
+    if (POLE[y][x] != 0) {
+        if (y - 1 >= 0) {
+            if (POLE[y - 1][x] === 0) {
+                swapDiv(x, y - 1, 'pole' + POLE[y][x], POLE[y][x]);
             }
         }
-        function findPosX(obj) {
-            var curleft = 0;
-            if (obj.offsetParent) {
-                while (1) {
-                    curleft+=obj.offsetLeft;
-                    if (!obj.offsetParent) {
-                        break;
-                    }
-                    obj=obj.offsetParent;
-                }
-            } else if (obj.x) {
-                curleft+=obj.x;
-            }
-            return curleft;
-        }
+        if (y + 1 < SIZE) {
+            if (POLE[y + 1][x] === 0) {
+                swapDiv(x, y + 1, 'pole' + POLE[y][x], POLE[y][x]);
 
-        function findPosY(obj) {
-            var curtop = 0;
-            if (obj.offsetParent) {
-                while (1) {
-                    curtop+=obj.offsetTop;
-                    if (!obj.offsetParent) {
-                        break;
-                    }
-                    obj=obj.offsetParent;
-                }
-            } else if (obj.y) {
-                curtop+=obj.y;
             }
-            return curtop;
         }
+        if (x - 1 >= 0) {
+            if (POLE[y][x - 1] === 0) {
+                swapDiv(x - 1, y, 'pole' + POLE[y][x], POLE[y][x]);
+            }
+        }
+        if (x + 1 < SIZE){
+            if(POLE[y][x + 1] === 0) {
+                swapDiv(x + 1, y, 'pole' + POLE[y][x], POLE[y][x]);
+            }
+        }
+    }
+}
+function startGame() {
+    addElements();
+}
+function random(set) {
+    var rec = -1;
+    set.add(16);
+    while (set.has(rec)) {
+        rec = Math.floor(Math.random() * SIZE * SIZE) + 1;
+    }
+    return rec;
+}
+function addElements() {
+    var a = new Set();
+    a.add(-1);
+    var my_div = document.getElementById("game");
+    var j = 0;
+    var maxRec = 0;
+    for(var i = 0; i < SIZE; i++) {
+        POLE[i] = [];
+        while (j < SIZE && maxRec < SIZE*SIZE - 1){
+            var rec = random(a);
+            a.add(rec);
+            POLE[i][j] = rec;
+            var div = document.createElement("div");
+            div.className = "point" + POLE[i][j];
+            div.innerHTML = "<h1>" + POLE[i][j] + "</h1>";
+            div.id = 'pole' + POLE[i][j];
+            my_div.appendChild(div);
+            j++;
+            maxRec++;
+        }
+        j = 0;
+    }
+    div = document.createElement("div");
+    div.className = "point0";
+    div.id = 'pole0';
+    my_div.appendChild(div);
+    POLE[SIZE-1][SIZE-1] = 0;
+}
+function findPosX(obj) {
+    var curleft = 0;
+    if (obj.offsetParent) {
+        while (1) {
+            curleft+=obj.offsetLeft;
+            if (!obj.offsetParent) {
+                break;
+            }
+            obj=obj.offsetParent;
+        }
+    } else if (obj.x) {
+        curleft+=obj.x;
+    }
+    return curleft;
+}
+
+function findPosY(obj) {
+    var curtop = 0;
+    if (obj.offsetParent) {
+        while (1) {
+            curtop+=obj.offsetTop;
+            if (!obj.offsetParent) {
+                break;
+            }
+            obj=obj.offsetParent;
+        }
+    } else if (obj.y) {
+        curtop+=obj.y;
+    }
+    return curtop;
+}
