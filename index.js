6d0b65892ce441ddad9a4dc5e10ea524227ff94c
SASHA-ASUS\Беляев <alexander_belyaev95@mail.ru>
2017-05-21 12:26:13 +0500

    Исправил замечания

--- index.js
+++ index.js
@@ -1,26 +1,65 @@
-var neighbours = {};
-
-function getNextCellId(cellId, cells) {
-    var nextCell = neighbours[cellId].filter(function(item) {
-        return parseInt(cells[item].value) === -1;
-    });
-    return nextCell.length !== 0 ? nextCell[0] : -1;
-}
+var gameField = [];
+var hasFieldUpdated = false;
+var ROW_SIZE = 4;
 
 function isWin(cells) {
     for (var i = 0; i < cells.length - 1; i++) {
-        if (parseInt(cells[i].value) !== i + 1) {
-            return false;
-        }
-    }
-    return true;
+         if (parseInt(cells[i].value) !== i + 1) {
+             return false;
+         }
+     }
+     return true;
 }
 
-function swapCells(nextCell, targetCell) {
-    nextCell.style.visibility = 'visible'; 
-    nextCell.value = targetCell.value;
-    targetCell.style.visibility = 'hidden'; 
-    targetCell.value = -1;
+function getPositionOnFieldById(id) {
+    return {
+        i: Math.floor(id/ROW_SIZE),
+        j: id % ROW_SIZE
+    };
+}
+
+function getIndexByPosition(i, j) {
+    return i*ROW_SIZE + j;
+}
+
+function swapCells(first, second) {
+    var temp = gameField[first.i][first.j];
+    gameField[first.i][first.j] = gameField[second.i][second.j];
+    gameField[second.i][second.j] = temp;
+}
+
+function updateGameField(cells, targetCell, winMessage) {
+    var targetPosition = getPositionOnFieldById(targetCell.id);
+    if (gameField[targetPosition.i][targetPosition.j] === -1) {
+        return;
+    }
+    var neighbours = getNeigbours(targetPosition);
+    hasFieldUpdated = false;
+    for(var i = 0; i < neighbours.length; i++) {
+        var position = neighbours[i];
+        if (gameField[position.i][position.j] === -1) {
+            hasFieldUpdated = true;
+            swapCells(position, targetPosition);
+        }
+    }
+}
+
+function getNeigbours(targetPosition) {
+    var neighboursPositions = [
+        { i: targetPosition.i, j: targetPosition.j + 1 },
+        { i: targetPosition.i, j: targetPosition.j - 1 },
+        { i: targetPosition.i + 1, j: targetPosition.j },
+        { i: targetPosition.i - 1, j: targetPosition.j }
+    ];
+    return neighboursPositions.filter(function(position) {
+        return isOnField(position.i, position.j);
+    });
+}
+
+function isOnField(i, j) {
+    return [i, j].every(function(el) {
+        return el >= 0 && el < 4;
+    });
 }
 
 function makeMove(event) {
@@ -32,14 +71,14 @@ function makeMove(event) {
     var playGround = document.getElementById('playground');
     var cells = playGround.getElementsByClassName('button');
     var winMessage = document.getElementById('win-message');
-    var nextCellId = getNextCellId(targetCell.id, cells);
-    if (nextCellId !== -1) {
-        swapCells(cells[nextCellId], targetCell);
-    }
-    if (isWin(cells)) {
-        winMessage.style.visibility = 'visible';
-    } else {
-        winMessage.style.visibility = 'hidden';
+    updateGameField(cells, targetCell, winMessage);
+    if (hasFieldUpdated) {
+        fillCells(cells);
+        if (isWin(cells)) {
+            winMessage.className = 'visible';
+        } else {
+            winMessage.className = 'hidden';
+        }
     }
 }
 
@@ -47,7 +86,6 @@ function getButton(id) {
     var button = document.createElement('input');
     button.id = id;
     button.type = 'button';
-    button.className = 'button';
     return button;
 }
 
@@ -77,27 +115,33 @@ function getSolvableCombination() {
     return cellsNumbers;
 }
 
-function getNeighbourCells(cellNumber) {
-    var neighbourCells = [];
-    if (cellNumber - 1 >= 0 && cellNumber % 4 !== 0) {
-        neighbourCells.push(parseInt(cellNumber - 1));
+function fillCells(buttons) {
+    for (var i = 0; i < ROW_SIZE; i++) {
+        for (var j = 0; j < ROW_SIZE; j++) {
+            var index = getIndexByPosition(i, j);
+            buttons[index].value = gameField[i][j];
+            buttons[index].className = 'button';
+            if (gameField[i][j] === -1) {
+                buttons[index].className += ' hidden';
+            }
+        }
     }
-    if (cellNumber + 1 < 16 && (cellNumber + 1) % 4 !== 0) {
-        neighbourCells.push(parseInt(cellNumber + 1));
+}
+
+function setGameField(positions) {
+    gameField = [];
+    for(var i = 0; i < ROW_SIZE; i++) {
+        gameField.push([]);
+        for(var j = 0; j < ROW_SIZE; j++) {
+            gameField[i].push(positions[getIndexByPosition(i, j)]);
+        }
     }
-    if (cellNumber - 4 >= 0) {
-        neighbourCells.push(parseInt(cellNumber - 4));
-    }
-    if (cellNumber + 4 < 16) {
-        neighbourCells.push(parseInt(cellNumber + 4));
-    }
-    return neighbourCells;
 }
 
 function setupGame() {
     var playGround = document.getElementById('playground');
     var winMessage = document.getElementById('win-message');
-    winMessage.style.visibility = 'hidden';
+    winMessage.className = 'hidden';
     var buttons = [];
     for (var i = 0; i < 16; i++) {
         var button = getButton(i);
@@ -107,20 +151,18 @@ function setupGame() {
             playGround.appendChild(document.createElement('br'));
         }
     }
-    buttons[0].style.visibility = 'hidden';
-    buttons[0].value = -1;
     if (playGround.addEventListener) {
         playGround.addEventListener('click', makeMove);
     } else {
         playGround.attachEvent('onclick', makeMove); // IE8
     }
-    var buttonsPositions = getSolvableCombination();
-    for (var j = 0; j < 16; j++) {
-        if (j > 0) {
-            buttons[j].value = buttonsPositions[j - 1];
-        }
-        neighbours[j] = getNeighbourCells(j);
-    }
+    var positions = getSolvableCombination();
+    setGameField([-1].concat(positions));
+    fillCells(buttons);
 }
 
-document.body.onload = setupGame();
+if (document.addEventListener) {
+    document.addEventListener('DOMContentLoaded', setupGame);
+} else {
+    document.attachEvent('onDOMContentLoaded', setupGame); // IE8
+}
--- tests/index-test.js
+++ tests/index-test.js
@@ -17,83 +17,88 @@ function clearPlayground() {
     playground.innerHTML = '';
 }
 
-function getWinCombination(cells) {
-    for (var i = 0; i < cells.length; i++) {
-        if (i === 14) {
-            cells[i].value = -1;
-            cells[i].style.visibility = 'hidden';
-        } else {
-            if (i === cells.length - 1) {
-                cells[i].value = i;
-            } else {
-                cells[i].value = i + 1;
-            }
-            cells[i].style.visibility = 'visible';
-        }
-    }
-    [].forEach.call(cells, function(item) {
-        neighbours[item.id] = getNeighbourCells(item.id);
+function isUniqueNumbers(numbers) {
+    var array = new Array(numbers.length);
+    for (var i = 0; i < array.length; array[i++] = 0);
+    numbers.forEach(function(item) {
+        array[item-1]++;
     });
-    return cells;
+    return array.every(function(item) {
+        return item === 1;
+    });
 }
 
-describe('Tag', function() {
+function hasClass(item, cls) {
+    return item.className.indexOf(cls) !== -1;
+}
+
+describe('Game tests', function() {
     var clickEvent = document.createEvent('Events');
     clickEvent.initEvent('click', true, false);
+    var cells;
+    var hiddenCell;
+
+    beforeEach(function() {
+        clearPlayground();
+        setupGame();
+        cells = getCells();
+        hiddenCell = getHiddenCell(cells);
+    });
 
     it('should create field 4x4 with 15 visible cells and one hidden', function() {
-        var cells = getCells();
         var visibleCells = cells.filter(function(item) {
             return parseInt(item.value) !== -1;
         });
         var hiddenCells = cells.filter(function(item) {
             return parseInt(item.value) === -1;
         });
+        var cellsNumbers = visibleCells.map(function(item) {
+            return parseInt(item.value);
+        });
 
         expect(visibleCells).to.have.lengthOf(15);
         expect(hiddenCells).to.have.lengthOf(1);
-        visibleCells.forEach(function(item, index) {
-            expect(document.getElementById((index + 1).toString())).to.eql(item);
-        });
+        expect(isUniqueNumbers(cellsNumbers)).to.be.true;
     });
 
     it('should move neighbour cell on empty by click', function() {
-        var cells = getCells();
-        var hiddenCell = getHiddenCell(cells);
+        setGameField([
+            -1, 1, 2, 3,
+            4, 5, 6, 7,
+            8, 9, 10, 11,
+            12, 13, 14, 15
+        ]);
+        fillCells(cells);
+        hiddenCell = cells[0];
         var expectedHiddenCellValue = hiddenCell.value;
-        var neighbour = cells[neighbours[hiddenCell.id][0]];
+        var neighbour = cells[1];
         var expectedNeighbourValue = neighbour.value;
 
         neighbour.dispatchEvent(clickEvent);
         expect(neighbour.value).to.eql(expectedHiddenCellValue);
-        expect(hiddenCell.style.visibility).to.equal('visible');
+        expect(hasClass(hiddenCell, 'hidden')).to.be.false;
         expect(hiddenCell.value).to.eql(expectedNeighbourValue);
-        expect(neighbour.style.visibility).to.equal('hidden');
+        expect(hasClass(neighbour, 'hidden')).to.be.true;
     });
 
     it('should not move empty cell by click', function() {
-        var cells = getCells();
-        var hiddenCell = getHiddenCell(cells);
         var expectedHiddenCellValue = hiddenCell.value;
 
         hiddenCell.dispatchEvent(clickEvent);
-        expect(hiddenCell.style.visibility).to.equal('hidden');
+        expect(hasClass(hiddenCell, 'hidden')).to.be.true;
         expect(hiddenCell.value).to.eql(expectedHiddenCellValue);
     });
 
     it('should not move cell that does not have empty cell in neighbours', function() {
-        var cells = getCells();
-        var hiddenCell = getHiddenCell(cells);
-        var cellsWithoutEmpty = cells.filter(function(item) {
-            return item.id !== hiddenCell.id;
-        });
-        var cell = cellsWithoutEmpty.filter(function(item) {
-            if (neighbours[item.id].every(function(el) {
-                return parseInt(cells[el].value) !== -1;
-            })) {
-                return item;
-            }
-        })[0];
+        setGameField([
+            -1, 1, 2, 3,
+            4, 5, 6, 7,
+            8, 9, 10, 11,
+            12, 13, 14, 15
+        ]);
+        fillCells(cells);
+        hiddenCell = cells[0];
+        var cell = cells[2];
         var expectedHiddenCellValue = hiddenCell.value;
         var expectedCellValue = cell.value;
 
@@ -103,15 +108,21 @@ describe('Tag', function() {
     });
 
     it('should show win message', function() {
-        var initialCells = getCells();
-        var cells = getWinCombination(initialCells);
+        setGameField([
+            1, 2, 3, 4,
+            5, 6, 7, 8,
+            9, 10, 11, 12,
+            13, 14, -1, 15
+        ]);
+        fillCells(cells);
+        hiddenCell = cells[cells.length - 2];
         var winMessage = document.getElementById('win-message');
 
-        expect(winMessage.style.visibility).to.equal('hidden');
+        expect(hasClass(winMessage, 'hidden')).to.be.true;
         cells[cells.length - 1].dispatchEvent(clickEvent);
-        expect(winMessage.style.visibility).to.equal('visible');
+        expect(hasClass(winMessage, 'visible')).to.be.true;
         
-        winMessage.style.visibility = 'hidden';
+        winMessage.className = 'hidden';
         clearPlayground();
         setupGame();
     });
